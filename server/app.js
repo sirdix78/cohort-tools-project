41514667c57b6d6ae76f2ebcaafd13a448149c25
Rojda Polat <rojdapolat@outlook.de>
2025-03-31 17:16:49 +0200

    api folder add

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-const cohorts = require("/api/cohorts.json");
-const students = require("/api/students.json");
+const cohorts = require("./api/cohorts.json");
+const students = require("./api/students.json");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
